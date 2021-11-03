49aa248c913e1e453dc372f43627f985f5e0d813
Saim Rizvi <shr2mz@gmail.com>
2021-11-03 00:51:28 -0400

    commit tree fdcbec4fefdc29369bc9cc9c56a3bcdaa9eb16d9

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,42 @@
-# your code goes here
+require "pry"
+class Person
+    attr_accessor :bank_account 
+    attr_reader :name, :happiness, :hygiene
+    def initialize (name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    def happiness=(num)
+        @happiness = if num > 10 
+                         10
+                    elsif num < 0
+                        0
+                    else 
+                        num
+                    end 
+    end
+    def hygiene=(num)
+        @hygiene =  if num > 10
+                        10
+                    elsif num < 0
+                        0
+                    else 
+                        num
+                    end
+    end
+
+    def happy?
+        self.happiness > 7 
+    end
+    def clean? 
+        self.hygiene > 7
+    end
+    
+
+
+        
+    
+end
+
